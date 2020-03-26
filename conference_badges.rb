821cedc0dceb7c12190b46399d126b24a92fc13c
Nicholas Keller <nic.keller328@gmail.com>
2020-03-26 17:25:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,15 @@
 def badge_maker(name)
-
   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.each do |name|
+    "Hello, my name is #{name}."
+end
+
+def assign_rooms(speakers)
+
+  speakers.each do |name|
+    "Hello, #{name}! You'll be assigned to room #{speakers"
 
 end
